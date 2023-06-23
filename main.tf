ce6d52bc61301dc6c486fb373aa69be0260e3927
Rudra <Rudrayyaswamims@gmail.com>
2023-06-23 12:41:48 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -30,6 +30,11 @@ resource "aws_route_table" "public_route_table" {
     { Name = "${var.env}-${each.value["name"]}" }
   )
 }
+## public  route table association
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.foo.id
+  route_table_id = aws_route_table.bar.id
+}
 
 ## private subnets
 resource "aws_subnet" "private_subnets" {
